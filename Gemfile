7988ce782baa6aa7e7c4535a36d715fc802c1fc4
ilham bouaik <ibouaik@gmail.com>
2022-11-14 14:18:06 +0000

    add devise

--- Gemfile
+++ Gemfile
@@ -75,3 +75,4 @@ group :test do
 end
 
 gem 'name_of_person'
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tweets, except: [:edit, :update] 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "tweets#index"
 end
