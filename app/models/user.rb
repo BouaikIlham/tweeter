8c4a8c8250186df3bd25fbfa6c7dc70f07990903
ilham bouaik <ibouaik@gmail.com>
2022-11-16 16:40:10 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_person_name
     has_many :tweets
+
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
 end
