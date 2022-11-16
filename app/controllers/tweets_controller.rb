176553b989c8b7e28d8684389916bc4ad6d900ea
ilham bouaik <ibouaik@gmail.com>
2022-11-16 12:19:32 +0000

    desplaying tweets using turbo

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,31 @@
 class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else 
+        format.html do
+          flash[:tweet.errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
