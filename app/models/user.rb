cb0846a2a10e01051fafcd196da3f627adc6c53f
ilham bouaik <ibouaik@gmail.com>
2022-11-17 00:05:34 +0000

    install active storage to attached profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, uniqueness: true
   validates :last_name, presence: true, uniqueness: true
+
+  has_one_attached :profile_image
 end
