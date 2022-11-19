f8543df1eddfc654dc6839e7dfa875d64b479f0d
ilham bouaik <ibouaik@gmail.com>
2022-11-19 20:47:31 +0000

    create coents model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweets, except: [:edit, :update] do
+    resources :comments, only: [:create, :destroy]
     member do
       post :retweet
     end
