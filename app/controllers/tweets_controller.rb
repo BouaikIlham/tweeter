ffbc770d1c449ee3da00a8046177795e75f5e087
ilham bouaik <ibouaik@gmail.com>
2022-11-19 12:53:53 +0000

    create retweet function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,6 +22,18 @@ class TweetsController < ApplicationController
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
+  end 
+  def retweet
+    @tweet = Tweet.find(params[:id])
+    retweet = current_user.tweets.new(tweet_id: @tweet.id)
+
+    respond_to do |format|
+      if retweet.save
+        format.turbo_stream
+      else 
+        format.html {redirect_back fallback_location: @tweet, alret: "Could not retweet"}
+      end
+    end
   end
 
 
