ed136dfc48e64195b0e7e1349a95f27b0357f25a
ilham bouaik <ibouaik@gmail.com>
2022-11-19 12:02:06 +0000

    create tweet type function

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,16 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  belongs_to :tweet, optional: true
 
   validates :body, length: {in: 6..1000}, allow_blank: false
+
+  def tweet_type
+    if tweet_id? && body?
+      "quote-tweet"
+    elsif tweet_id?
+      "retweet"
+    else
+      "tweet"
+    end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tweets, except: [:edit, :update] 
+  resources :tweets, except: [:edit, :update] do
+    member do
+      post :retweet
+    end
+  end
   resources :profiles
   root "tweets#index"
 end
