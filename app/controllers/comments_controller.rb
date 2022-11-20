0543d4975c534fd0284b8bba655002c08d985365
ilham bouaik <ibouaik@gmail.com>
2022-11-20 22:31:12 +0000

    create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,36 @@
 class CommentsController < ApplicationController
-    def create
+   before_action :set_tweet
+
+  def create
+    @comment = @tweet.comments.new(comment_params.merge(user: current_user))
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+      else
+        format.html { redirect_to tweet_path(@tweet), alert: "Reply could not be created" }
+      end
+    end
+  end
+    def destroy
+        @comment = @tweet.comments(params[:id])
+        @comment.destroy
+        respond_to do |format|
+            if @comment.save
+                format.turbo_stream
+            else 
+                format.html {redirect_to tweet_path(@tweet), alert: "comment was deleted"}
+            end 
+        end    
     end
 
-    def destroy
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
+
+    def set_tweet
+        @tweet = Tweet.find(params[:tweet_id])
     end
 end
